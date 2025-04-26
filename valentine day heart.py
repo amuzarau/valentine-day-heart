9ab9f8bcd4f03b90faaa437d342877dcf1b97427
Andrei Muzarau <95071115+amuzarau@users.noreply.github.com>
2025-04-26 23:56:34 +0300

    Add files via upload

---
+++ valentine day heart.py
@@ -0,0 +1,9 @@
+# Write your code here :-)
+print('\n'.join
+ ([''.join
+   ([('Valya'[(x-y)%5 ]
+     if((x*0.05)**2+(y*0.1)**2-1)
+      **3-(x*0.05)**2*(y*0.1)
+       **3<=0 else ' ')
+        for x in range(-30,30)])
+         for y in range(15,-15,-1)]))
